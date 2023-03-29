119feef6cc2c9c641385674ab57787f228482c30
Rafael Santos <rafaelchaves@unifei.edu.br>
2023-03-28 22:58:40 -0300

    refactor: tira os comentarios

--- MoviesApi/src/StickerCreator.java
+++ MoviesApi/src/StickerCreator.java
@@ -12,31 +12,23 @@ public class StickerCreator {
 
     public void create(InputStream inputStream, String archiveName) throws IOException {
 
-        // leitura da imagem
-        // FileInputStream inputStreeam = new FileInputStream(new
-        // File("input/filme.jpg"));
-        // inputStream = new URL(
-        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
         BufferedImage originalImage = ImageIO.read(inputStream);
 
-        // cria nova imagem em memoria comt ranspoarecnaio e com tamanho novo
         int width = originalImage.getWidth();
         int height = originalImage.getHeight();
         int newHeight = height + 200;
 
         BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 
-        // copiar a imagem original para nova imagem (em memória)
         Graphics2D graphics = (Graphics2D) newImage.getGraphics();
 
         graphics.drawImage(originalImage, 0, 0, null);
 
-        // escrever uma frase na nova imagem
         var font = new Font(Font.SANS_SERIF, Font.BOLD, 80);
         graphics.setFont(font);
         graphics.setColor(Color.YELLOW);
-        graphics.drawString("TopZera", 0, newHeight - 20);
-        // escrever a imagem nova em um arquivo
+        graphics.drawString("TopZera", width / 2, newHeight - 100);
+
         ImageIO.write(newImage, "png", new File("output/" + archiveName.replace(':', '-')));
 
     }
